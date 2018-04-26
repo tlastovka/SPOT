670ac3b2c4e45280bfc4fea9bba4ced27ccdbbdb
Tomas <tl@premiato.org>
2018-04-26 23:18:31 +0200

    the sport.js set up, imported and working. Initial props in place, spot.css file created and imported, all linked to git

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
+import {Spot} from "./spots/spot";
+
+
 
 class App extends React.Component {
 	render() {
 		return (
+			<div>
 			<h1>Hello, from the SPOT!</h1>
+			<Spot spotId="1"/>   {/* render Spot of spotIt=1 */}
+			</div>
 		)
 	}
 }
 
 ReactDOM.render(
 	<App />,
+
 	document.getElementById('app')
 )
---
+++ src/spots/spot.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './spot.css';
+
+export class Spot extends React.Component {
+    constructor (props){
+        super(props);
+       // <!-------start of block .bind(this)---------!-->
+
+
+
+       // <!-------end of block .bind(this)---------!-->
+        this.state = {
+            spotId:     props.spotId,
+            spotX:      0,       //Geo positioning coordinates X of the respective Spot
+            spotY:      0,       //Geo positioning coordinates y of the respective Spot
+            minTariff:  0,   //Minimum stated tariff set by Admin - it may not equal to the amount paid due to other constraints e.g. various discounts,
+            maxTariff:  0,   //Minimum stated tariff set by Admin - it may not equal to the amount paid due to other constraints e.g. various discounts,
+            sweetStartTariff:   0, //Tariff applicable at sweetStartOccup as defined by the Admin
+            sweetEndTariff:     0, //Tariff applicable at sweetEndOccup as defined by the Admin
+            sweetStartOccup:    props.sweetStartOccup, //A lower limit treshold where the occupancy should ideally be - cost go up (default value = 70%).
+            sweetEndOccup:      props.sweetEndOccup, //A top limit treshold where the occupancy should ideally be - cost go up even steeper (default value = 90%).
+            permitDiscount:     0, //Tariff discount applicable to permit holders (police, medical assistance, emergency, etc) 0=0%, 1 = 100%
+            residentDiscount:   0, //Tariff discount applicable to the registered local residents (procedure to be clarified) 0=0%, 1 = 100%
+            offPeakDiscount:    0, //Tariff discount applicable during off peak hours (night, weekends) 0=0%, 1 = 100%
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Spot No.: {this.state.spotId} </h2>  {/* I want to show Id of the spot! */}
+                <p>Coordinates: X:{this.state.spotX} Y:{this.state.spotY}</p>
+                <p>Coordinates: X:{this.state.sweetStartOccup} Y:{this.state.sweetEndOccup}</p>
+            </div>
+        )
+    }
+}
+
+Spot.defaultProps ={
+    sweetStartOccup:    0.70,
+    sweetEndOccup:      0.90,
+    permitDiscount:     1.0,
+};
